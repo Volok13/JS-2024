36280ed225a0b6997c4d2db2296720ac85be5401
Volok13 <volokhova2017@gmail.com>
2024-08-05 21:39:39 +0300

    hm1

---
+++ lesson1/lesson1.1/lesson1.1.js
@@ -0,0 +1,7 @@
+let firstName    = 'Ann';
+let middleName = 'Andreevna';
+let lastName = 'Bichenko';
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+// let person = `${firstName} ${middleName} ${lastName}`
+console.log(person);
